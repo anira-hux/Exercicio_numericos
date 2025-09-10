6664b5b96628d82b6a23a60cc5cde6bdcae30e3d
anira-hux <anitahux.killer@gmail.com>
2025-09-10 19:37:08 -0300

    dever de númericos

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Exercicio_numericos
+
+- Exercício da primeira aula de numéricos com o João no primeiro semestre. 
+- O objetivo era programar um código em C++ que calculasse a integral númerica de uma função utilizanod pontos conhecidos atraez do metódo dos trapézios. (esses pontos precisavam ter o mesmo passo - senod asima mesma distancia entre eles) 
+- O seguinte código aproximou sin(x) para o intervalo 0 a pi com o número de subintervalos variáveis mediante escolha do usuário. 
+
+- Feito por Anita Cunha
+- NUSP:16983549
+- Data: 10/09/2025
---
+++ integral_numerica.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+double aproximação_trapezios(double (*f)(double), double a, double b, int n) {
+    double h = (b - a) / n;
+    double soma = 0.5 * (f(a) + f(b));
+    
+    for (int i = 1; i < n; ++i) {
+        soma += f(a + i * h);
+    }
+    
+    return soma * h;
+}
+
+double f(double x) {
+    return std::sin(x); // Exemplo de função a ser integrada
+}
+
+int main() {
+    double a = 0.0; // Limite inferior
+    double b = M_PI; // Limite superior
+    int n; // Número de subintervalos
+
+    std::cout << "Digite o número de subintervalos (n): ";
+    std::cin >> n;
+
+    double resultado = aproximação_trapezios(f, a, b, n);
+
+    // integral extata de sin(x) de 0 a pi é 2
+
+    double v_exato = 2.0;
+
+    //calculo do erro exato 
+
+    double erro_exato = std::abs(v_exato - resultado);
+    
+    std::cout << "Aproximação da integral usando " << n << " intervalos: " << resultado << std::endl;
+    std::cout << "Valor exato da integral: " << v_exato << std::endl;
+    std::cout << "Erro exato usando " << n << " intervalos: " << erro_exato << std::endl;
+
+    return 0;
+}
+
+// Exemplo de uso: integrar sin(x) de 0 a pi
+
