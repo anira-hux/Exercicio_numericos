33a113477b7aa2cd499f94a2af6eb0f6fa4631d5
anira-hux <anitahux.killer@gmail.com>
2025-09-16 09:16:55 -0300

    Atualização incluindo a estimativa do erro

--- integral_numerica.cpp
+++ integral_numerica.cpp
@@ -33,10 +33,14 @@ int main() {
     //calculo do erro exato 
 
     double erro_exato = std::abs(v_exato - resultado);
+
+    double erro_estimado = (std::pow((b - a), 3) / (12 * std::pow(n, 2))) * 1; // |f''(x)| <= 1 para sin(x)
     
     std::cout << "Aproximação da integral usando " << n << " intervalos: " << resultado << std::endl;
     std::cout << "Valor exato da integral: " << v_exato << std::endl;
     std::cout << "Erro exato usando " << n << " intervalos: " << erro_exato << std::endl;
+    std::cout << "Erro estimado usando " << n << " intervalos: " << erro_estimado << std::endl;    
+    std::cout << "Diferença entre erro exato e estimado: " << std::abs(erro_exato - erro_estimado) << std::endl; 
 
     return 0;
 }
